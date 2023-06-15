ad683d88b55b6b853dd5678b3ac25ede0d56ea08
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-15 12:43:03 +0200

    defined static nav in appheader

--- vue-3-header/src/components/AppHeader.vue
+++ vue-3-header/src/components/AppHeader.vue
@@ -1,18 +1,26 @@
 <template>
-    <div>
-        
-    </div>
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Products</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            navItemLinks: [],
-        }
-    },
-}
+    export default {
+        data() {
+            return {
+                navItemLinks: [],
+            }
+        },
+    }
 </script>
+
 <style lang="scss">
     
 </style>
