174f5f53162e789c1b121ccd7e68bc5169084a29
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-15 12:41:02 +0200

    defined data in script added new array

---
+++ vue-3-header/src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            navItemLinks: [],
+        }
+    },
+}
+</script>
+<style lang="scss">
+    
+</style>
